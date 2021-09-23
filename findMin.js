d9b7b557ea30885742c0cd57547045405113c02f
maggiezhao11 <maggiezhao06@hotmail.com>
2021-09-23 17:37:31 -0400

    update array

--- findMin.js
+++ findMin.js
@@ -1,13 +1,13 @@
-const arr = [64, 25, 12, 22, 11];
 
 //set min = arr[0] is to in order to include the first elements in the array to be able to being compared.
+const arr = [11, 25, 12, 22, 64];
 let min = arr[0]; 
 
 const findMin = function (array) {
 	
-	for(let i = 0; i < array.length - 1; i++) {
-		if(array[i+1] < array[i]) {
-			min = array[i+1];		
+	for(let i = 0; i < array.length; i++) {
+		if(min > array[i]) {
+			min = array[i];		
 		}
 	}
 	
