c136ae0b60c1066d05edb92bd2e64c0e6538cc9c
maggiezhao11 <maggiezhao06@hotmail.com>
2021-09-23 17:35:57 -0400

    add explaination for findmin

--- findMin.js
+++ findMin.js
@@ -1,4 +1,6 @@
 const arr = [64, 25, 12, 22, 11];
+
+//set min = arr[0] is to in order to include the first elements in the array to be able to being compared.
 let min = arr[0]; 
 
 const findMin = function (array) {
